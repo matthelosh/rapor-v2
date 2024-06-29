4bb4f4e5133587bed6e80084e9c96dc9f79efb59
matthelosh <matthelosh@gmail.com>
2024-06-29 16:24:22 +0700

    Fix Dynamic dashboard component

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,17 +2,16 @@
 import {ref, computed, defineAsyncComponent } from 'vue'
 import DashLayout from '@/Layouts/DashLayout.vue';
 import { Head, usePage } from '@inertiajs/vue3';
-import { ElCard } from 'element-plus'
-import { Icon } from '@iconify/vue'
-import { capitalize } from '@/helpers/String.js'
+
+const Admin = defineAsyncComponent(() => import('@/Components/Dashboard/Home/Admin.vue'))
+const Ops = defineAsyncComponent(() => import('@/Components/Dashboard/Home/Ops.vue'))
+const Wali = defineAsyncComponent(() => import('@/Components/Dashboard/Home/Wali.vue'))
+const Mapel = defineAsyncComponent(() => import('@/Components/Dashboard/Home/Mapel.vue'))
 
 const page = usePage()
 const role = page.props.auth.roles[0]
 const comp = computed(() => {
-    return role == 'admin' ? 'Admin' : (role == 'ops' ? 'Ops' : (role == 'guru_kelas' ? 'Wali' : 'Mapel'))
-})
-const is = computed(() => {
-    return defineAsyncComponent(() => import('../Components/Dashboard/Home/'+comp.value+'.vue'))
+    return role == 'admin' ? Admin : (role == 'ops' ? Ops : (role == 'guru_kelas' ? Wali : Mapel))
 })
 
 const sekolahs = page.props.sekolahs
@@ -26,7 +25,7 @@ const sekolahs = page.props.sekolahs
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
-        <component :is="is" class="page">
+        <component :is="comp" class="page">
         </component>
     </DashLayout>
 </template>
