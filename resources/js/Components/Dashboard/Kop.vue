a070e729274b29a1b49e8ccb6c50ec89091a68c0
matthelosh <matthelosh@gmail.com>
2024-10-13 09:00:22 +0700

    Fix Kop Rapor KEcamatan

--- resources/js/Components/Dashboard/Kop.vue
+++ resources/js/Components/Dashboard/Kop.vue
@@ -21,7 +21,7 @@ const defaultLogo = (e) => {
         <div class="teks text-center font-serif col-span-6 print:col-span-8">
             <h3 class="uppercase font-bold text-lg print:text-[12pt]">Pemerintah Kabupaten Malang</h3>
             <h3 class="uppercase font-bold text-lg print:text[10pt]">Dinas Pendidikan dan Kebudayaan</h3>
-            <h3 class="uppercase font-bold text print:text-[10pt]">Korwil Dinas Pendidikan Kecamatan Wagir</h3>
+            <h3 class="uppercase font-bold text print:text-[10pt]">Korwil Dinas Pendidikan Kecamatan {{  capitalize(sekolah.kecamatan) }}</h3>
             <h1 class="uppercase font-bold text-2xl print:text-[14pt]">{{ sekolah.nama }}</h1>
             <p class="font-light print:text-[10pt]">Alamat: {{ capitalize(sekolah.alamat )}}, {{ capitalize(sekolah.desa) }}, Kec. {{ capitalize(sekolah.kecamatan) }}, Kode Pos {{ sekolah.kode_pos }}</p>
             <p class="font-light print:text-[10pt]">Telp: {{ capitalize(sekolah.telp )}}, Email: {{ sekolah.email }}, Website: {{ sekolah.website }}</p>
