4f4bef4a1edabda11941a6b277848a6faa3dd605
Muhammad Soleh <matthelosh@gmail.com>
2025-05-20 13:57:22 +0700

    Tweak Kop

--- resources/js/Components/Dashboard/Kop.vue
+++ resources/js/Components/Dashboard/Kop.vue
@@ -28,26 +28,24 @@ const defaultLogo = (e) => {
             />
         </div>
         <div class="teks text-center font-serif col-span-6 print:col-span-8">
-            <h3 class="uppercase font-bold text-lg print:text-[12pt]">
+            <h3 class="uppercase font-bold text-lg print:text-[12pt] leading-5">
                 Pemerintah Kabupaten Malang
             </h3>
-            <h3 class="uppercase font-bold text-lg print:text[10pt]">
+            <h3 class="uppercase font-bold text-lg print:text[10pt] leading-5">
                 Dinas Pendidikan dan Kebudayaan
             </h3>
-            <h3 class="uppercase font-bold text print:text-[10pt]">
+            <h3 class="uppercase font-bold text print:text-[10pt] leading-5">
                 Korwil Dinas Pendidikan Kecamatan
                 {{ capitalize(sekolah.kecamatan) }}
             </h3>
-            <h1 class="uppercase font-bold text-2xl print:text-[14pt]">
+            <h1 class="uppercase font-bold text-2xl print:text-[14pt] leading-5">
                 {{ sekolah.nama }}
             </h1>
-            <p class="font-light print:text-[10pt]">
-                Alamat: {{ capitalize(sekolah.alamat) }},
-                {{ capitalize(sekolah.desa) }}
-                {{ capitalize(sekolah.kecamatan) }}, Kode Pos
+            <p class="font-light print:text-[10pt] leading-5">
+                Alamat: {{ capitalize(sekolah.alamat) }}, {{ capitalize(sekolah.desa) }}, Kode Pos
                 {{ sekolah.kode_pos }}
             </p>
-            <p class="font-light print:text-[10pt]">
+            <p class="font-light print:text-[10pt] leading-5">
                 Telp: {{ capitalize(sekolah.telp) }}, Email:
                 {{ sekolah.email }}, Website: {{ sekolah.website }}
             </p>
