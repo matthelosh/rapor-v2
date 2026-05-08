e2a55f98db6aa9d6ee47cda38c2a283b0f9845af
matthelosh <matthelosh@gmail.com>
2026-05-08 13:29:59 +0700

    hmsms..

--- resources/views/cetak/kartupelajar.blade.php
+++ resources/views/cetak/kartupelajar.blade.php
@@ -84,7 +84,7 @@
                 width: 100%;
                 height: 100%;
                 object-fit: cover;
-                object-position: 50% 10%;
+                object-position: 50% 8%;
             }
         }
 
