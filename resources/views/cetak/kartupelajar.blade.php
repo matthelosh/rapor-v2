9707cf67809c2b66cb81b774502e4e7ed233d13a
matthelosh <matthelosh@gmail.com>
2026-05-08 13:27:44 +0700

    Hmmmm...

--- resources/views/cetak/kartupelajar.blade.php
+++ resources/views/cetak/kartupelajar.blade.php
@@ -84,8 +84,7 @@
                 width: 100%;
                 height: 100%;
                 object-fit: cover;
-                /* transform: translateY(8%); */
-                object-position: 50% 50%;
+                object-position: 50% 20%;
             }
         }
 
